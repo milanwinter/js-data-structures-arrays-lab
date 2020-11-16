0ec597605b255d9c2db04672798a8e4b40a26836
milanwinter <milanwinter8@gmail.com>
2020-11-16 15:17:00 -0800

    commit tree 58dfc7593153ffc688966a2a12efb72a4c75013b

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const drivers = ["Milo","Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name)
+    drivers
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name)
+    drivers
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop()
+    drivers
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift()
+    drivers
+}
+
+function appendDriver(name) {
+  const driversTwo = [ ...drivers , name];
+  return driversTwo
+}
+
+function prependDriver(name) {
+    const driversThree = [name, ...drivers]
+    return driversThree
+}
+
+function removeLastDriver() {
+    return drivers.slice(0, drivers.length - 1);
+}
+
+function removeFirstDriver() {
+    return drivers.slice(1)
+}
